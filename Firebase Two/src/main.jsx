a2f7e2adfd962ca71e5470bff2c10f189ed3ae95
showrov4g <showrovg00@gmail.com>
2024-11-09 21:44:50 +0600

    learing Firebase

--- Firebase Two/src/main.jsx
+++ Firebase Two/src/main.jsx
@@ -1,14 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./Layout/MainLayout.jsx";
+import Home from "./Components/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>,
+      }
+    ]
   },
 ]);
 
