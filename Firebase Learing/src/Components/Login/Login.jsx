4328d6f3d052331e84313810c7fcac494304d5e4
showrov4g <showrovg00@gmail.com>
2024-11-09 11:01:27 +0600

    Learning Firebase

--- Firebase Learing/src/Components/Login/Login.jsx
+++ Firebase Learing/src/Components/Login/Login.jsx
@@ -1,23 +1,57 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import auth from "../../firebase/firebase.init";
-
+import { useState } from "react";
 
 const Login = () => {
-    const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState(null);
+
+  const provider = new GoogleAuthProvider();
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-    .then(results =>{
-        console.log(results);
-    })
-    .catch(error =>{
+      .then((result) => {
+        console.log(result);
+        setUser(result);
+      })
+      .catch((error) => {
         console.log(error);
-    })
+        setUser(null);
+      });
+  };
+  const githubSignin=()=>{
+    
+  }
+
+
+
+
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .than(() => {
+        console.log("Sign out successfully");
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
   };
 
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Create account with Google</button>
+      {user ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <div>
+          <button onClick={handleGoogleSignIn}>Create account with Google</button>
+          <button onClick={githubSignin}> login with  github</button>
+        </div>
+      )}
+
+      {user && (
+        <div>
+          <h4>{user.displayName}</h4>
+          <p>{user.email}</p>
+        </div>
+      )}
     </div>
   );
 };
